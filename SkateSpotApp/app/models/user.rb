d1e357a09b2af7453d7598676a1c0e6d2a63ad17
alitopaloglu <ali_t@gwmail.gwu.edu>
2015-05-10 18:16:56 -0400

    minor fixes

--- SkateSpotApp/app/models/user.rb
+++ SkateSpotApp/app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
 	
 	has_many :posts
 
+	validates :email, :encrypted_password presence: true
+
 	devise :database_authenticatable, :registerable,
 		   :recoverable, :rememberable, :trackable, :validatable
 end
