096607b4617a776c8e1b94736da86c645beb08a2
alitopaloglu <ali_t@gwmail.gwu.edu>
2015-05-11 11:46:25 -0400

    minor additions to spots

--- SkateSpotApp/app/controllers/spots_controller.rb
+++ SkateSpotApp/app/controllers/spots_controller.rb
@@ -2,10 +2,15 @@ class SpotsController < ApplicationController
 
 	def index
 		@spots = Spot.all
+		# @city = @spot.city
+		# @address = @spot.address
+		# @description = @spot.description
 
 	end
 
 	def show
+		@spot = Spot.find(params[:id])
+		
 
 	end
 
@@ -17,4 +22,25 @@ class SpotsController < ApplicationController
 	def create
 
 	end
+
+	private 
+		def spots_params
+			params.require(:spot).permit(
+				:city,
+				:address,
+				:description,
+				:smoothness,
+				:spot,
+				:human_traffic,
+				:security
+				)
+		end
+	
 end
+		# t.string :city
+  #   	t.string :address, :null => false
+  #   	t.string :description
+  #   	t.integer :smoothness
+  #   	t.integer :spot
+  #   	t.integer :human_traffic
+  #   	t.integer :security
