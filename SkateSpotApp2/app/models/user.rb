dc67684f0cc817909ea9723dec49c771d9fa0fc5
alitopaloglu <ali_t@gwmail.gwu.edu>
2015-05-12 16:06:12 -0400

    user authentication

--- SkateSpotApp2/app/models/user.rb
+++ SkateSpotApp2/app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ActiveRecord::Base
-
 	has_many :posts
 	has_many :spots
-	
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+
+	# Virtual attribute for authenticating by either username or email
+	# This is in addition to a real persisted field like 'username'
+	# attr_accessor :login
+	# Include default devise modules. Others available are:
+		# :confirmable, :lockable, :timeoutable and :omniauthable
+  	devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable #, :authentication_keys => {email: true, login: false}
 end
