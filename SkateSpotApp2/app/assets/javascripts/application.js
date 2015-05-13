fe07220d53e80a011574fbe27f0fde0362071588
alitopaloglu <ali_t@gwmail.gwu.edu>
2015-05-13 17:36:49 -0400

    API to DC

--- SkateSpotApp2/app/assets/javascripts/application.js
+++ SkateSpotApp2/app/assets/javascripts/application.js
@@ -14,13 +14,15 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-$(document).ready(function(){
-	var map = L.map('map').setView([51.505, -0.09], 13);
-	L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
-    	attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
-    	subdomains: ['otile1','otile2','otile3','otile4']
-	}).addTo( map );
-	var marker = L.marker([51.5, -0.09]).addTo(map);
+// $(document).ready(function(){
+// 	var map = L.map('map').setView([38.9040, -77.065], 13);
+// 	L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
+//     	attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
+//     	subdomains: ['otile1','otile2','otile3','otile4']
+// 	}).addTo( map );
+// 	var marker = L.marker([38.9040, -77.065]).addTo(map);
+
+//	map.addControl( new L.Control.Search({layer: searchLayer}) );
     // since layers load asynchronously through AJAX, use the
     // `.on` function to listen for them to be loaded before
     // calling `getTileJSON()`
@@ -28,5 +30,27 @@ $(document).ready(function(){
     //     // get TileJSON data from the loaded layer
     //     var TileJSON = layer.getTileJSON();
     // });
-})
+// })
+window.onload = function () {
+		var map = L.map('map').setView([38.90, -77.03], 12);
+ 	
+		L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+			attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+		}).addTo(map);
+
+		var countryStyle = {
+			'color': "#000",
+			'weight': 2,
+			'opacity': 0.6
+		};
+	
+		function popup(feature, layer) {
+			if (feature.properties && feature.properties.name) {
+				layer.bindPopup(feature.properties.name);
+			}
+		}
+		var marker = L.marker([38.90, -77.03]).addTo(map);
+
+
+	};
 
