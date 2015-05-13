1745582bd9823437f8ceca3ca1b41cee2e21ff4d
alitopaloglu <ali_t@gwmail.gwu.edu>
2015-05-13 11:05:01 -0400

    simple API

--- SkateSpotApp2/app/assets/javascripts/application.js
+++ SkateSpotApp2/app/assets/javascripts/application.js
@@ -14,3 +14,19 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+$(document).ready(function(){
+var map = L.map('map').setView([51.505, -0.09], 13);
+// var layer = L.mapbox.tileLayer('https://api.tiles.mapbox.com/v3/examples.map-0l53fhk2.json');
+L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
+    subdomains: ['otile1','otile2','otile3','otile4']
+}).addTo( map );
+    // since layers load asynchronously through AJAX, use the
+    // `.on` function to listen for them to be loaded before
+    // calling `getTileJSON()`
+    // .on('ready', function() {
+    //     // get TileJSON data from the loaded layer
+    //     var TileJSON = layer.getTileJSON();
+    // });
+})
+
