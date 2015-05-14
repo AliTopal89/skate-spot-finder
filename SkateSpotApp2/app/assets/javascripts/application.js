7aa9c4429e9486a23b462b34f6dedf6a3a85338d
alitopaloglu <ali_t@gwmail.gwu.edu>
2015-05-13 20:43:09 -0400

    almost done with the spots page

--- SkateSpotApp2/app/assets/javascripts/application.js
+++ SkateSpotApp2/app/assets/javascripts/application.js
@@ -14,23 +14,7 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-// $(document).ready(function(){
-// 	var map = L.map('map').setView([38.9040, -77.065], 13);
-// 	L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
-//     	attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
-//     	subdomains: ['otile1','otile2','otile3','otile4']
-// 	}).addTo( map );
-// 	var marker = L.marker([38.9040, -77.065]).addTo(map);
 
-//	map.addControl( new L.Control.Search({layer: searchLayer}) );
-    // since layers load asynchronously through AJAX, use the
-    // `.on` function to listen for them to be loaded before
-    // calling `getTileJSON()`
-    // .on('ready', function() {
-    //     // get TileJSON data from the loaded layer
-    //     var TileJSON = layer.getTileJSON();
-    // });
-// })
 window.onload = function () {
 		var map = L.map('map').setView([38.90, -77.03], 12);
  	
